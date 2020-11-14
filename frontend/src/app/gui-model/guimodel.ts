c07b0a4f24d4d5f92d9e678073f7f6ff246f86d3
erismmic <erismmic@students.zhaw.ch>
2020-11-14 14:52:44 +0000

    Version 11 Willkommen angepasst

--- frontend/src/app/gui-model/guimodel.ts
+++ frontend/src/app/gui-model/guimodel.ts
@@ -1492,7 +1492,7 @@ export class GuiModel {
                     "elementList": [
                         {
                             "type": "pageLabel",
-                            "value": "<h4>Willkommen bei DevOps</h4>",
+                            "value": "<h4>Seite von Michel Erismann Studiengang</h4>",
                             "newRow": true
                         },
                         {
